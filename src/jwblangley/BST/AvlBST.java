74127249b0a089a9c43ac1c8e87a5f8ec90ccb49
jwblangley <jwblangley@gmail.com>
2018-04-24 19:45:24 +0100

    add implemented - needs testing

--- src/jwblangley/BST/AvlBST.java
+++ src/jwblangley/BST/AvlBST.java
@@ -7,23 +7,57 @@ public class AvlBST<T extends Comparable<T>> implements BinarySearchTree<T> {
   Node<T> root = null;
 
   @Override
-  public boolean add(T obj) {
+  public synchronized boolean add(T obj) {
+    Node<T> newNode = new Node<>(obj);
+    if (root == null) {
+      root = newNode;
+      return true;
+    }
+    Node<T> curr = root;
+    Node<T> parent = root;
+    while (curr != null) {
+      if (obj.compareTo(curr.getItem()) == 0) {
+        //item already in tree
+        return false;
+      } else {
+        parent = curr;
+        curr = (obj.compareTo(curr.getItem()) < 0) ? curr.getLeft() : curr.getRight();
+      }
+    }
+
+    if ((obj.compareTo(parent.getItem()) < 0)) {
+      parent.setLeft(newNode);
+    } else {
+      parent.setRight(newNode);
+    }
+
+    rebalance();
     return false;
   }
 
   @Override
-  public boolean remove(T obj) {
+  public synchronized boolean remove(T obj) {
+    //TODO @James: implement remove
     return false;
   }
 
   @Override
-  public int size() {
+  public synchronized int size() {
+    assert isValid();
+    //TODO @James: implement size
     return 0;
   }
 
   @Override
-  public List<T> inOrderTraversal() {
+  public synchronized List<T> inOrderTraversal() {
+    assert isValid();
+    //TODO @James: implement inOrderTraversal
     return null;
+  }
+
+  private void rebalance() {
+    //TODO @James: implement rebalance
+    assert isValid();
   }
 
 
@@ -32,11 +66,17 @@ public class AvlBST<T extends Comparable<T>> implements BinarySearchTree<T> {
   }
 
   private boolean isValidInner(Node<T> currentNode) {
-    //TODO @James: implement BST invariant check
     if (Math.abs(height(currentNode.getLeft()) - height(currentNode.getRight())) > 1) {
+      System.out.println("AVL invariant failed");
       return false;
     }
-    return isValidInner(currentNode.getLeft()) && isValidInner(currentNode.getRight());
+    if (currentNode.getItem().compareTo(currentNode.getLeft().getItem()) > 0
+        && currentNode.getItem().compareTo(currentNode.getRight().getItem()) < 0) {
+      return isValidInner(currentNode.getLeft()) && isValidInner(currentNode.getRight());
+    } else {
+      System.out.println("BST invariant failed");
+      return false;
+    }
   }
 
   private int height(Node root) {
