c9c89db3eea52dbb692b281a82f4f0a3cebb3f2c
jwblangley <jwblangley@gmail.com>
2018-04-24 19:03:06 +0100

    Initial commit

---
+++ src/jwblangley/BST/AvlBST.java
@@ -0,0 +1,26 @@
+package jwblangley.BST;
+
+import java.util.List;
+
+public class AvlBST<T> implements BinarySearchTree<T>{
+
+  @Override
+  public boolean add(T obj) {
+    return false;
+  }
+
+  @Override
+  public boolean remove(T obj) {
+    return false;
+  }
+
+  @Override
+  public int size() {
+    return 0;
+  }
+
+  @Override
+  public List<T> inOrderTraversal() {
+    return null;
+  }
+}
---
+++ src/jwblangley/BST/BinarySearchTree.java
@@ -0,0 +1,15 @@
+
+package jwblangley.BST;
+
+import java.util.List;
+
+public interface BinarySearchTree<T extends Comparable<T>> {
+
+  public boolean add(T obj);
+
+  public boolean remove(T obj);
+
+  public int size();
+
+  public List<T> inOrderTraversal();
+}
